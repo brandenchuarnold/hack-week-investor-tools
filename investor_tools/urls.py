8356fa23a06a67ceb0b759525ec47d31dbbe9777
Rohan Alur <rohana@zillowgroup.com>
2016-07-21 16:46:31 -0700

    thursday afternoon

--- investor_tools/urls.py
+++ investor_tools/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, url
 
-from investor_tools.views import HomePageView
+from investor_tools.views import home
 
 urlpatterns = patterns(
     '',
 
-    url(r'', HomePageView.as_view(), name='home'),
+    url(r'', home),
 )
--- investor_tools/views.py
+++ investor_tools/views.py
@@ -1,20 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.base import View
+from django.shortcuts import render
+import json
+from django.core.serializers.json import DjangoJSONEncoder
 
+def home(request, *args, **kwargs):
 
-class HomePageView(View):
+    cap_hill_properties = [{'address' : 'fake address', 'latitude' : 47.6062, 'longitude' : -122.3321}]
+    south_lake_properties = [{'address' : 'fake address 2', 'latitude' : 47.6092, 'longitude' : -122.3371}, {'address' : 'fake address 3', 'latitude' : 47.6052, 'longitude' : -122.3311}]
+    neighborhood_list = [
+        {'name' : 'Capitol Hill', 'id' : 'capitol_hill', 'properties' : cap_hill_properties, 'neighborhood_attributes' : {}}, 
+        {'name' : 'South Lake Union', 'id' : 'south_lake_union', 'properties' : south_lake_properties, 'neighborhood_attributes' : {}}, 
+        {'name' : 'Beacon Hill', 'id' : 'beacon_hill','properties' : [], 'neighborhood_attributes' : {}},
+        {'name' : 'University District', 'id' : 'university_district','properties' : [], 'neighborhood_attributes' : {}},
+        {'name' : 'Queen Anne', 'id' : 'queen_anne','properties' : [], 'neighborhood_attributes' : {}},
+        {'name' : 'Pioneer Square', 'id' : 'pioneer_square','properties' : [], 'neighborhood_attributes' : {}},
+        {'name' : 'West Seattle', 'id' : 'west_seattle','properties' : [], 'neighborhood_attributes' : {}},
+        {'name' : 'Magnolia', 'id' : 'magnolia','properties' : [], 'neighborhood_attributes' : {}}
+    ]
 
-    def dispatch(request, *args, **kwargs):
-        response_text = '''\
-            <html>
-            <head>
-                <title>Investor Tools</title>
-            </head>
-            <body>
-                <h1>Investor Tools</h1>
-                <p>Hello, world!</p>
-            </body>
-            </html>
-        '''
-        return HttpResponse(response_text)
+    neighborhood_dict = {}
+    for neighborhood in neighborhood_list:
+        neighborhood_dict[neighborhood['id']] = neighborhood
+
+    context = {'neighborhood_dict' : neighborhood_dict, 'neighborhood_json' : json.dumps(neighborhood_dict)}
+
+    return render(request, "index.html", context)
