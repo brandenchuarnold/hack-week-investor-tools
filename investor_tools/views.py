4983056a77b28bd4561217cf79efdb0ab2e543bf
Rohan Alur <rohana@zillowgroup.com>
2016-07-25 13:24:42 -0700

    added neighborhood data

--- investor_tools/views.py
+++ investor_tools/views.py
@@ -44,7 +44,7 @@ def get_real_data(neighborhoods):
 	neighborhood_dict = {}
 	for _id, name in neighborhoods.items():
 		neighborhood_attributes = json.loads(requests.get('http://localhost:5000/neighborhood_data/' + str(_id)).content)
-		neighborhood = {'name' : name, 'id' : name.lower().replace(' ', '_'), 'neighborhood_attributes' : neighborhood_attributes}
+		neighborhood = {'name' : name, 'id' : name.lower().replace(' ', '_'), 'neighborhood_attributes' : neighborhood_attributes['neighborhood_data']}
 
 		property_list = json.loads(requests.get('http://localhost:5000/property_region/' + str(_id)).content)
 		formatted_properties = []
@@ -69,7 +69,7 @@ def get_mortgage_rate():
 		return mortgage_rate
 
 	except KeyError:
-		return '4'
+		return '4.5'
 
 def home(request, *args, **kwargs):
 	neighborhoods = {250206 : 'Capitol Hill', 250017 : 'Ballard', 250692 : 'Freemont', 252248 : 'Wallingford', 
